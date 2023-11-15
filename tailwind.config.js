00c10310dd61e60d7da7b691e3e6f0cdced84070
Evelyn Santos <117581658+eveffc@users.noreply.github.com>
2023-11-15 12:30:50 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        cinza1:'#d7dbdd',
+        cinza2:'#918d8a',
+        grafite:'#333333',
+      },
+    },
+  },
+  plugins: [],
+}
+
